2d2b597e583d4b18014d8a7542ee8dc42aff0703
LH-studio <lucien.haase@gmail.com>
2026-03-12 22:37:08 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mouth-mixer-v7';
+const CACHE_NAME = 'mouth-mixer-v9';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', e => {
